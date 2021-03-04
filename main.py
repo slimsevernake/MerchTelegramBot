b699fb3c80740ec0ced45f98c08d3739631993be
ApXNTekToP <apxntektopp@gmail.com>
2021-03-05 02:56:31 +0400

    Добавил приветствие при нажатии кнопочки /start

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import telebot
 
-
 token = "1614577997:AAHECoJ6qH6DrKS-MNO1WSUc9HZ5RFr512c"
 bot = telebot.TeleBot(token)
 
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message,
+                     "Lamp oil? Rope? Bombs? You want it? It's your's, my friend, as long as you have enough rupees.")
 
+
+bot.polling()
