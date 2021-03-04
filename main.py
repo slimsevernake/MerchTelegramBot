5c26cdb298c4b3e05fa480220ea32f607e5402dd
ApXNTekToP <apxntektopp@gmail.com>
2021-03-04 23:26:34 +0400

    Basic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot
 
-bot = telebot.TeleBot("1614577997:AAHECoJ6qH6DrKS-MNO1WSUc9HZ5RFr512c")
+
+token = "1614577997:AAHECoJ6qH6DrKS-MNO1WSUc9HZ5RFr512c"
+bot = telebot.TeleBot(token)
 
 
 
