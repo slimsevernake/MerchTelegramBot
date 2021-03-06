1a9e3f945023fcd59236a7d06ac96f7c7c025c13
ApXNTekToP <apxntektopp@gmail.com>
2021-03-06 14:59:39 +0400

    Добавление класса User

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ token = "1614577997:AAHECoJ6qH6DrKS-MNO1WSUc9HZ5RFr512c"
 bot = telebot.TeleBot(token)
 
 
+
+class User:
+    def __init__(self):
+        self.chat_id = message.from_user.id
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message,
-                     "Lamp oil? Rope? Bombs? You want it? It's your's, my friend, as long as you have enough rupees.")
+    chat_id = message.from_user.id
+    bot.send_message(chat_id,
+                     'Lamp oil? Rope? Bombs? You want it? It\'s your\'s, '
+                     'my friend, as long as you have enough rupees.')
+    User()
 
 
 bot.polling()
