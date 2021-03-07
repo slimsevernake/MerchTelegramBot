4eeea1a8444e5306786236dbcd214e471a86ed5e
ApXNTekToP <apxntektopp@gmail.com>
2021-03-07 15:56:24 +0400

    Добавлено подключение к базе данных.

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+# Import
 import telebot
+import mysql.connector
+from mysql.connector import Error
 
-token = "1614577997:AAHECoJ6qH6DrKS-MNO1WSUc9HZ5RFr512c"
-bot = telebot.TeleBot(token)
+# Bot Connect
+token_bot = "1614577997:AAHECoJ6qH6DrKS-MNO1WSUc9HZ5RFr512c"
+bot = telebot.TeleBot(token_bot)
+
+
+# MySQL Connect
+def create_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+connection = create_connection("localhost", "root", "Qsf98%x$")
 
 
 user_data = {}
@@ -13,16 +36,16 @@ class User(object):
         user_data['Last_name'] = self.last_name = last_name
         user_data['Phone'] = self.phone = phone
         user_data['Chat_id'] = self.chat_id = chat_id
+        print(user_data)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    first_name = message.from_user.first_name
-    last_name = message.from_user.last_name
-    chat_id = message.from_user.id
-    phone = getattr(message, 'form_user.phone', 'false')
-    user_object = User(first_name, last_name, chat_id, phone)
-    bot.send_message(chat_id,
+    user_object = User(message.from_user.first_name,
+                       message.from_user.last_name,
+                       message.from_user.id,
+                       getattr(message, 'form_user.phone', 'false'))
+    bot.send_message(user_data['Chat_id'],
                      'Lamp oil? Rope? Bombs? You want it? It\'s your\'s, '
                      'my friend, as long as you have enough rupees.')
 
