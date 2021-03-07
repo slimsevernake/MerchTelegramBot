5cf24d86daa15d12fffacf4331b7184de277fe37
ApXNTekToP <apxntektopp@gmail.com>
2021-03-07 22:28:02 +0400

    Добавил таблицы в базу данных.

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ bot = telebot.TeleBot(token_bot)
 
 
 # MySQL Connect
-def create_connection(host_name, user_name, user_password):
+def create_connection(host_name, user_name, user_password, database_name=""):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            database=database_name
         )
         print("Connection to MySQL DB successful")
     except Error as e:
@@ -25,20 +26,55 @@ def create_connection(host_name, user_name, user_password):
 
 
 connect_database = create_connection("localhost", "root", "Qsf98%x$")
+print(connect_database)
 
 
 # Create DataBase
-def create_database(connection, query):
+def create_database(connection):
     cursor = connection.cursor()
     try:
-        cursor.execute(query)
+        create_database_query = "CREATE DATABASE merch_telegram_bot_db"
+        cursor.execute(create_database_query)
+        query_table = "CREATE TABLE customer (" \
+                      "id INT AUTO_INCREMENT PRIMARY KEY," \
+                      "first_name VARCHAR(255)," \
+                      "last_name VARCHAR(255)," \
+                      "email VARCHAR(255)," \
+                      "phone VARCHAR(255)," \
+                      "chat_id INT UNIQUE)"
+        connect_db = create_connection("localhost",
+                                       "root",
+                                       "Qsf98%x$",
+                                       "merch_telegram_bot_db")
+        cursor = connect_db.cursor()
+        cursor.execute(query_table)
+        query_table = "CREATE TABLE product (" \
+                      "id INT AUTO_INCREMENT PRIMARY KEY," \
+                      "name VARCHAR(255)," \
+                      "description TEXT," \
+                      "price INT)"
+        cursor.execute(query_table)
+        query_table = "CREATE TABLE product_photo (" \
+                      "id INT AUTO_INCREMENT PRIMARY KEY," \
+                      "url VARCHAR(255)," \
+                      "product_id INT REFERENCES product(id))"
+        cursor.execute(query_table)
+        query_table = "CREATE TABLE cart (" \
+                      "id INT AUTO_INCREMENT PRIMARY KEY," \
+                      "customer_id INT REFERENCES customer(id))"
+        cursor.execute(query_table)
+        query_table = "CREATE TABLE cart_product (" \
+                      "cart_id INT REFERENCES cart(id)," \
+                      "product_id INT REFERENCES product(id))"
+        cursor.execute(query_table)
         print("Database created successfully")
+        return connect_db
     except Error as e:
         print(f"The error '{e}' occurred")
 
 
-create_database_query = "CREATE DATABASE merch_telegram_bot_db"
-create_database(connect_database, create_database_query)
+connect_database = create_database(connect_database)
+print(connect_database)
 
 
 user_data = {}
