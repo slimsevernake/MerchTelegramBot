edd0d133e35dbf35188801663312e33d2bd8cbff
ApXNTekToP <apxntektopp@gmail.com>
2021-04-29 20:51:58 +0400

    Удаление MySQL, переход проекта на PostgreSQL

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # Import
 import telebot
 from telebot import types
-import mysql.connector
-from mysql.connector import Error
 
 # Bot Connect
 token_bot = "1614577997:AAHECoJ6qH6DrKS-MNO1WSUc9HZ5RFr512c"
@@ -25,100 +23,6 @@ class Product(object):
 
 class Category(object):
     pass
-
-
-# MySQL Connect
-def create_connection(host_name, user_name,
-                      user_password, database_name=""):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=database_name
-        )
-        print("Connection to MySQL DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
-
-
-connect_database = create_connection("localhost", "root", "Qsf98%x$")
-
-
-# Create DataBase
-def create_database(connection):
-    cursor = connection.cursor()
-    try:
-        create_database_query = "CREATE DATABASE merch_telegram_bot_db"
-        cursor.execute(create_database_query)
-        query_table = "CREATE TABLE customer (" \
-                      "id INT AUTO_INCREMENT PRIMARY KEY," \
-                      "first_name VARCHAR(255)," \
-                      "last_name VARCHAR(255)," \
-                      "email VARCHAR(255)," \
-                      "phone VARCHAR(255)," \
-                      "chat_id INT UNIQUE)"
-        connect_db = create_connection("localhost",
-                                       "root",
-                                       "Qsf98%x$",
-                                       "merch_telegram_bot_db")
-        cursor = connect_db.cursor()
-        cursor.execute(query_table)
-        query_table = "CREATE TABLE category (" \
-                      "id INT AUTO_INCREMENT PRIMARY KEY," \
-                      "category_name VARCHAR(255))"
-        cursor.execute(query_table)
-        query_table = "CREATE TABLE product (" \
-                      "id INT AUTO_INCREMENT PRIMARY KEY," \
-                      "category_id INT," \
-                      "name VARCHAR(255)," \
-                      "description TEXT," \
-                      "price INT," \
-                      "FOREIGN KEY (category_id) REFERENCES category(id))"
-        cursor.execute(query_table)
-        query_table = "CREATE TABLE product_photo (" \
-                      "id INT AUTO_INCREMENT PRIMARY KEY," \
-                      "url VARCHAR(255)," \
-                      "product_id INT," \
-                      "FOREIGN KEY (product_id) REFERENCES product(id))"
-        cursor.execute(query_table)
-        query_table = "CREATE TABLE cart (" \
-                      "id INT AUTO_INCREMENT PRIMARY KEY," \
-                      "customer_id INT," \
-                      "FOREIGN KEY(customer_id) REFERENCES customer(id))"
-        cursor.execute(query_table)
-        query_table = "CREATE TABLE cart_product (" \
-                      "cart_id INT," \
-                      "product_id INT," \
-                      "FOREIGN KEY(product_id) REFERENCES product(id)," \
-                      "FOREIGN KEY(cart_id) REFERENCES cart(id))"
-        cursor.execute(query_table)
-        print("Database created successfully")
-        return connect_db
-    except Error as e:
-        connect_db = create_connection("localhost",
-                                       "root",
-                                       "Qsf98%x$",
-                                       "merch_telegram_bot_db")
-        print(f"The error '{e}' occurred")
-        return connect_db
-
-
-connect_database = create_database(connect_database)
-
-
-# Insert DataBase
-def insert_database(connection, query, value):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query, value)
-        connection.commit()
-        print("Insert completed")
-    except Error as e:
-        print(f"The error '{e}' occurred")
 
 
 # Buttons
